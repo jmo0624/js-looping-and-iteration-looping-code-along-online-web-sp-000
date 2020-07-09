b6a38b57742e423cefbf41e564daa8d830f99c96
Josh Morris <jmo0624@gmail.com>
2020-07-09 23:23:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function writeCards( namesArray, event ) {
     return thankYouCards
   }
 
-  function countdown( countdown ) {
-    while ( countdown > 0 ) {
-      console.log( countdown );
-      countdown -= 1;
-    }
+function countdown( countdown ) {
+  while ( countdown > 0 ) {
     console.log( countdown );
+    countdown -= 1;
   }
+  console.log( countdown );
+}
